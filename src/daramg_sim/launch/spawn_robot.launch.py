a116640972edac24dcc0af2d4367f45580502061
kimhoyun-robotair <suberkut76@gmail.com>
2025-05-14 00:21:02 +0900

    Add Gazebo Classic sim and Gazebo Harmonic Sim. In Gazebo Harmonic, We can do RTAB-MAP sim

--- src/daramg_sim/launch/spawn_robot.launch.py
+++ src/daramg_sim/launch/spawn_robot.launch.py
@@ -1,81 +1,210 @@
-import launch
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
-from launch_ros.actions import Node
 import os
-from launch.actions import DeclareLaunchArgument
-
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # path to the robot model
-    pkg_share = launch_ros.substitutions.FindPackageShare(
-        package='daramg_sim'
-    ).find('daramg_sim')
-    default_model_path = os.path.join(pkg_share, 'urdf/simple_rover.urdf')
 
-    # param for using simulation time
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    pkg_simple_rover = get_package_share_directory('daramg_sim')
+    pkg_share = FindPackageShare(package='daramg_sim').find('daramg_sim')
 
-    # Position and orientation for spawning robot in gazebo
-    # [X, Y, Z]
-    position = [0.0, 0.0, 0.0]
-    # [Roll, Pitch, Yaw]
-    orientation = [0.0, 0.0, 0.0]
+    gazebo_models_path, ignore_last_dir = os.path.split(pkg_simple_rover)
+    os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
+    joy_teleop_config_file = os.path.join(pkg_share, 'config/MXswitch.config.yaml')
 
-    declare_use_sim_time_cmd = DeclareLaunchArgument(
-        name='use_sim_time',
-        default_value='true',
-        description='Use simulation (Gazebo) clock if true'
+    """
+    rviz_launch_arg = DeclareLaunchArgument(
+        'rviz', default_value='true',
+        description='Open RViz'
+    )
+
+    rviz_config_arg = DeclareLaunchArgument(
+        'rviz_config', default_value='rviz.rviz',
+        description='RViz config file'
+    )
+    """
+
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='world.sdf',
+        description='Name of the Gazebo world file to load'
+    )
+
+    model_arg = DeclareLaunchArgument(
+        'model', default_value='simple_rover.urdf',
+        description='Name of the URDF description to load'
+    )
+
+    x_arg = DeclareLaunchArgument(
+        'x', default_value='2.5',
+        description='x coordinate of spawned robot'
+    )
+
+    y_arg = DeclareLaunchArgument(
+        'y', default_value='1.5',
+        description='y coordinate of spawned robot'
+    )
+
+    yaw_arg = DeclareLaunchArgument(
+        'yaw', default_value='-1.5707',
+        description='yaw angle of spawned robot'
+    )
+
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='True',
+        description='Flag to enable use_sim_time'
+    )
+
+    # Define the path to your URDF or Xacro file
+    urdf_file_path = PathJoinSubstitution([
+        pkg_simple_rover,  # Replace with your package name
+        "urdf",
+        LaunchConfiguration('model')  # Replace with your URDF or Xacro file
+    ])
+
+    world_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_simple_rover, 'launch', 'world.launch.py'),
+        ),
+        launch_arguments={
+        'world': LaunchConfiguration('world'),
+        }.items()
+    )
+    """
+    # Launch rviz
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', PathJoinSubstitution([pkg_simple_rover, 'rviz', LaunchConfiguration('rviz_config')])],
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+    """
+
+    # Spawn the URDF model using the `/world/<world_name>/create` service
+    spawn_urdf_node = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", "mogi_bot",
+            "-topic", "robot_description",
+            "-x", LaunchConfiguration('x'), "-y", LaunchConfiguration('y'), "-z", "0.2", "-Y", LaunchConfiguration('yaw')  # Initial spawn position
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
+    # Node to bridge /cmd_vel and /odom
+    gz_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+            "/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry",
+            "/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model",
+            "/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
+            #"/camera/image@sensor_msgs/msg/Image@gz.msgs.Image",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
+            "/imu@sensor_msgs/msg/Imu@gz.msgs.IMU",
+            #"/navsat@sensor_msgs/msg/NavSatFix@gz.msgs.NavSat",
+            "/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan",
+            #"/scan/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
+            #"/camera/depth_image@sensor_msgs/msg/Image@gz.msgs.Image",
+            "/camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
+    # Node to bridge camera image with image_transport and compressed_image_transport
+    gz_image_bridge_node = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=[
+            "/camera/image",
+            "/camera/depth_image"
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time'),
+             'camera.image.compressed.jpeg_quality': 75},
+        ],
+    )
+
+    # Relay node to republish /camera/camera_info to /camera/image/camera_info
+    relay_camera_info_node = Node(
+        package='topic_tools',
+        executable='relay',
+        name='relay_camera_info',
+        output='screen',
+        arguments=['camera/camera_info', 'camera/image/camera_info'],
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
     )
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        emulate_tty=True,
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': Command(['xacro', ' ', urdf_file_path]),
+             'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ],
+        remappings=[
+            ('/tf', 'tf'),
+            ('/tf_static', 'tf_static')
+        ]
+    )
+
+    joy = Node(
+        package='joy',
+        executable='joy_node',
+        name='joy_node',
         parameters=[{
-            'use_sim_time': use_sim_time,
-            'robot_description': Command(['xacro ', LaunchConfiguration('model')])
+            'device_id': 0,          # ls /dev/input/js* to find the device id
+            'deadzone': 0.3,         # Deadzone for the joystick axes
+            'autorepeat_rate': 20.0, # Autorepeat rate for the joystick buttons
         }]
     )
 
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'use_sim_time': use_sim_time}],
+    joy_teleop = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_twist_joy_node',
+        parameters=[joy_teleop_config_file]
     )
 
-    spawn_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-entity', 'racecar',
-            '-x', str(position[0]),
-            '-y', str(position[1]),
-            '-z', str(position[2]),
-            '-R', str(orientation[0]),
-            '-P', str(orientation[1]),
-            '-Y', str(orientation[2]),
-            '-topic', '/robot_description'
-        ],
-        output='screen'
-    )
+    launchDescriptionObject = LaunchDescription()
 
-    return launch.LaunchDescription([
-        DeclareLaunchArgument(
-            name='model',
-            default_value=default_model_path,
-            description='Absolute path to robot urdf file'
-        ),
-        launch.actions.ExecuteProcess(
-            cmd=[
-                'gazebo', '--verbose', '-s',
-                'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'
-            ],
-            output='screen'
-        ),
-        declare_use_sim_time_cmd,
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        spawn_entity
-    ])
+    #launchDescriptionObject.add_action(rviz_launch_arg)
+    #launchDescriptionObject.add_action(rviz_config_arg)
+    launchDescriptionObject.add_action(world_arg)
+    launchDescriptionObject.add_action(model_arg)
+    launchDescriptionObject.add_action(x_arg)
+    launchDescriptionObject.add_action(y_arg)
+    launchDescriptionObject.add_action(yaw_arg)
+    launchDescriptionObject.add_action(sim_time_arg)
+    launchDescriptionObject.add_action(world_launch)
+    #launchDescriptionObject.add_action(rviz_node)
+    launchDescriptionObject.add_action(spawn_urdf_node)
+    launchDescriptionObject.add_action(gz_bridge_node)
+    launchDescriptionObject.add_action(gz_image_bridge_node)
+    launchDescriptionObject.add_action(relay_camera_info_node)
+    launchDescriptionObject.add_action(robot_state_publisher_node)
+    launchDescriptionObject.add_action(joy)
+    launchDescriptionObject.add_action(joy_teleop)
+
+    return launchDescriptionObject
